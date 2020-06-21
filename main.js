df3bcd1a639379b7a124f5032f1fc3969308ba7b
JILLBAI <tiger90483@gmail.com>
2020-06-21 16:26:35 +0800

    gy

---
+++ main.js
@@ -0,0 +1,15 @@
+AOS.init();
+$("#banner").slick({
+   autoplay:true,
+   autoplaySpeed:2000,
+   arrows:false
+});
+$("#brand").slick({
+ slidesToShow:3,
+ slidesToScroll:3,
+ autoplay:true,
+ autoplaySpeed:2000,
+ dots:true,
+ variableWidth:true
+
+});
